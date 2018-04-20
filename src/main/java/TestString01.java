61e75e15ecb3cfc9bb4bbb4b8ad376086e66d7fe
mxudsz <mxudsz@163.com>
2018-04-20 13:28:53 +0800

    TestString

---
+++ src/main/java/TestString01.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class TestString01 {
+    @Test
+    public void testString(){
+        String str ="hello,world";
+        System.out.println(str.substring(2,5));
+    }
+}
