65b58322f5a04f66496a1191a15f5e0fc8c34e93
mxudsz <mxudsz@163.com>
2018-04-20 14:38:01 +0800

    testbyte

---
+++ src/main/java/TestByte01.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class TestByte01 {
+    @Test
+    public void testByte01() {
+        byte b01 = (byte) 11111;
+        byte b02 = 111;
+        System.out.println(b01);
+        System.out.println(b02);
+
+
+    }
+}
